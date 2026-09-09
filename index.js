d7469be81991a1076cdbbd4cebcc2723790fd093
Anna <anna@hypership.tech>
2026-09-09 17:13:56 +0100

    Completed the hover effect

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const container = document.querySelector(".container")
+
 function createDiv(){
     const div = document.createElement("div");
-    div.classList.add("square");
+    div.classList.add("oneSquare");
     return div;
     
 }
@@ -11,4 +12,14 @@ for(i=0; i<256; i++){
     container.appendChild(createDiv());
 }
 
+// Makes the grid
+
+const allSquares = document.querySelectorAll(".oneSquare");
+allSquares.forEach((square)=>{
+    square.addEventListener("mouseenter", (event) => {
+        const hoveredDiv = event.target;
+        hoveredDiv.style.backgroundColor = "blue";
+
+    });
+});
 
