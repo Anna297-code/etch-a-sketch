7282028d7b256df42882b0886be64ed659b49674
Anna <anna@hypership.tech>
2026-09-10 17:08:09 +0100

    Added hover effect

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ function createGrid(width){
     for(let i=0; i < width ** 2; i++){
         const square = document.createElement("div");
         square.classList.add("oneSquare");
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "blue";
+        });
         square.style.height = `${640/width}px`;
         square.style.width = `${640/width}px`;
         container.appendChild(square);
@@ -17,7 +20,7 @@ function createGrid(width){
 }
 
 const button = document.createElement("button");
-button.textContent = "Restart";
+button.textContent = "New Grid";
 button.classList.add("button");
 body.prepend(button);
 
@@ -26,7 +29,7 @@ button.addEventListener("click", () => {
     let number = Number(userInput);
 
     if(number > 100){
-        alert("Number must be less than 100");
+        alert("Number must be less than 100 or less");
     } else {
          container.innerHTML = "";
         createGrid(number);
